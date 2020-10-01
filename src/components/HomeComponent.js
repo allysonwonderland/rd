d33700767203fd9ff1527a11154c9cec29988894
Ally <allyson.lifter@gmail.com>
2020-09-30 23:02:27 -0400

    nj

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -11,13 +11,24 @@ function Home(props) {
 				</div>
 			</div>
 			<div className="row row-content">
-				<div className="col">Hi friends! Welcome to the Rainbow Drop website. We are a top 10 alliance on Phantasy Star Online 2 Global Servers Ship 2 (UR)! Most of the information found on this website can be accessed through our discord, but we wanted to provide another source of information distribution for our current (and future!) members.</div>
+				<div className="col">
+					Hi friends! Welcome to the Rainbow Drop website. We are a top 10
+					alliance on Phantasy Star Online 2 Global Servers Ship 2 (UR)! Most of
+					the information found on this website can be accessed through our
+					discord, but we wanted to provide another source of information
+					distribution for our current (and future!) members.
+				</div>
 			</div>
-      <div className="row">
+			<div className="row">
 				<div className="col">
 					<h2>News</h2>
 					<hr />
 				</div>
+			</div>{" "}
+			<div className="row row-content">
+				<div className="col">
+
+        </div>
 			</div>
 		</div>
 	);
