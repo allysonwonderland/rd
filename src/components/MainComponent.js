d84cbd7df57ca0ab064f01cca50bf52ca94f4be6
Ally <allyson.lifter@gmail.com>
2020-10-02 23:22:39 -0400

    _redirects

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,29 +6,29 @@ import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Apply from "./ApplyComponent";
 import About from "./AboutComponent";
-import Resources from './ResourcesComponent';
-import Calendar from './CalendarComponent';
-import { Switch, Route, Redirect } from "react-router-dom";
+import Resources from "./ResourcesComponent";
+import Calendar from "./CalendarComponent";
+import { Switch, Route, Redirect, Router } from "react-router-dom";
 
 class Main extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/calendar" component={Calendar} />
-          <Route path="/about" component={About} />
-          <Route path="/rules" component={Rules} />
-          <Route path="/roster" component={Roster} />
-          <Route path="/apply" component={Apply} />
-          <Route path="/resources" component={Resources} />
-          <Redirect to="/home" />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+					<Header />
+					<Switch>
+						<Route path="/home" component={Home} />
+						<Route path="/calendar" component={Calendar} />
+						<Route path="/about" component={About} />
+						<Route path="/rules" component={Rules} />
+						<Route path="/roster" component={Roster} />
+						<Route path="/apply" component={Apply} />
+						<Route path="/resources" component={Resources} />
+						<Redirect to="/home" />
+					</Switch>
+					<Footer />
+			</div>
+		);
+	}
 }
 
 export default Main;
